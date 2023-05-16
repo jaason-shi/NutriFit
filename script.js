ccd3780179c5a47812bc883d60307c28a2cde2eb
jaason-shi <jasons8002@gmail.com>
2023-05-16 09:46:52 -0700

    create basic js file for snake game

--- script.js
+++
@@ -1,4 +0,0 @@
-const playBoard = document.querySelector(".play-board");
-const scoreElement = document.querySelector(".score");
-const highScoreElement = document.querySelector(".high-score");
-const controls = document.querySelectorAll(".controls i");
