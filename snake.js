fa5ee973db31d335f02d967fc43e86ff63b6468f
jaason-shi <jasons8002@gmail.com>
2023-05-16 13:40:52 -0700

    create function to initialize the game

--- snake.js
+++ snake.js
@@ -53,3 +53,52 @@ controls.forEach((button) =>
     changeDirection({ key: button.dataset.key })
   )
 );
+
+// Initializing the game
+const initGame = () => {
+  if (gameOver) return handleGameOver();
+  let html = `<div class="food" style="grid-area: ${foodY} / ${foodX}"></div>`;
+
+  // Checking if the snake hit the food
+  if (snakeX === foodX && snakeY === foodY) {
+    updateFoodPosition();
+    snakeBody.push([foodY, foodX]);
+    score++;
+    highScore = score >= highScore ? score : highScore;
+    localStorage.setItem("high-score", highScore);
+    scoreElement.innerText = `Score: ${score}`;
+    highScoreElement.innerText = `High Score: ${highScore}`;
+  }
+
+  // Updating the snake's head position based on the current velocity
+  snakeX += velocityX;
+  snakeY += velocityY;
+
+  // Shifting the values of the elements in the snake body by one
+  for (let i = snakeBody.length - 1; i > 0; i--) {
+    snakeBody[i] = snakeBody[i - 1];
+  }
+
+  // Setting first element of snake body to current snake position
+  snakeBody[0] = [snakeX, snakeY];
+
+  // Checking if the snake's moves past the wall, if so setting gameOver to true
+  if (snakeX <= 0 || snakeX > 30 || snakeY <= 0 || snakeY > 30) {
+    return (gameOver = true);
+  }
+
+  // Adding a div for each part of the snake's body
+  for (let i = 0; i < snakeBody.length; i++) {
+    html += `<div class="head" style="grid-area: ${snakeBody[i][1]} / ${snakeBody[i][0]}"></div>`;
+
+    // Checking if the snake head hit the body, if so set gameOver to true
+    if (
+      i !== 0 &&
+      snakeBody[0][1] === snakeBody[i][1] &&
+      snakeBody[0][0] === snakeBody[i][0]
+    ) {
+      gameOver = true;
+    }
+  }
+  playBoard.innerHTML = html;
+};
